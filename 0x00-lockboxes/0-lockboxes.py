48117c61cf92204b5abda3504baebc3c2832a414
tabbykatz <tomelay@gmail.com>
2020-12-07 09:09:17 -0800

    Task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" lockboxes algorithm """
+
+
+def canUnlockAll(boxes):
+    """ Check to see if it's possible to unlock all these boxes """
+    keys, search = [], []
+    keys.append(0)  # we have the 0th key
+
+    while keys:  # get the next key
+        current = keys.pop()
+
+        if current not in search:
+            search.append(current)  # add current to list searched
+            for key in boxes[current]:  # add keys to our keys list
+                keys.append(key)
+
+    return len(search) == len(boxes)  # have we searched all the rooms? T/F
